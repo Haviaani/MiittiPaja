7103983844b7171d6a6d100f31e6c3a9eb3abd10
Aki Haviala <p89565@edu.sasky.fi>
2025-09-26 13:40:15 +0300

    Hakemistoviittaukset vakioiksi.

--- config/config.php
+++ config/config.php
@@ -4,5 +4,11 @@
             "baseurl" => "/~p89565/lanify"
         )
     );
+
+    define("PROJECT_ROOT", dirname(__DIR__) . "/");
+    define("HELPERS_DIR", PROJECT_ROOT . "src/helpers/");
+    defire("TEMPLATE_DIR", PROJECT_ROOT . "src/view/");
+    define("MODER_DIR", PROJECT_ROOT . "src/model/");
+    defire("CONTROLLER_DIR", PROJECT_ROOT . "src/controller/");
     
 ?> 
