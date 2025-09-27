203d26510f27241f5b08024e5e4024d20e0625e6
Aki Haviala <p89565@edu.sasky.fi>
2025-09-27 19:42:15 +0300

    Linkitykset tapahtuma-sivulle ja etusivulle

--- src/view/tapahtumat.php
+++ src/view/tapahtumat.php
@@ -12,6 +12,7 @@
         echo "<div>";
             echo "<div>$tapahtuma[nimi]</div>";
             echo "<div>" . $start->format('j.n.Y') . "-" . $end->format('j.n.Y') . "</div>";
+            echo "<div><a href='tapahtuma?id=" . $tapahtuma['idtapahtuma'] . "'>TIEDOT</a></div>";
         echo "</div>";
     }
 ?>
