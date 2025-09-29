bca296d3a936f744dc428568a5ee33b051124e01
Aki Haviala <p89565@edu.sasky.fi>
2025-09-29 13:35:10 +0300

    Henkilön lisääminen tietokantaan

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@
         case '/':
         case '/tapahtumat':
             require_once MODEL_DIR . 'tapahtuma.php';
-            $tapahtumat = haeTapahtuma();
+            $tapahtumat = haeTapahtumat();
             echo $templates->render('tapahtumat', ['tapahtumat' => $tapahtumat]);
             break;
         case '/tapahtuma':
@@ -31,8 +31,16 @@
             }
             break;
         case '/lisaa_tili':
+            if (isset($_POST['laheta'])) {
+                require_once MODEL_DIR . 'henkilo.php';
+                $salasana = password_hash($_POST['salasana'], PASSWORD_DEFAULT);
+                $id = lisaaHenkilo($_POST['nimi'],$_POST['email'],$_POST['discord'],$salasana);
+                echo "Tili on luotu tunnisteella $id";
+                break;
+            } else {
             echo $templates->render('lisaa_tili');
             break;
+            }
         default:
             echo $templates->render('notfound');
     }
